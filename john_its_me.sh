b3c82ff92b5cece23fd3ed65be2f7e414f37d5f1
DoronV <ec2-user@ip-172-31-41-42.eu-central-1.compute.internal>
2018-03-09 19:59:36 +0000

    Made the story interesting

--- john_its_me.sh
+++ john_its_me.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 echo "It's me, I'm in the machine!"
+echo "Will you help John?"
+echo "y/n"
+read answer
+if [[ $answer == y  ]] ; then
+	echo "Thank you"
+else
+	echo "Why god, whyyyyyyyyyy"
+fi
